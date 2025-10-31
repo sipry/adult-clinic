b674b5b0fe1b5490542a3ff0f1ead5f2380bc430
sipry <qsole@protonmail.com>
2025-10-31 12:34:29 -0400

    Wrap AllServices component in Suspense for improved loading state handling

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Suspense } from "react";
 import { useRouter } from "next/navigation";
 import Navbar from "@/app/components/Navbar";
 import AllServices from "@/app/services/components/AllServices";
@@ -7,12 +8,16 @@ import Footer from "../components/Footer";
 
 export default function ServicesPage() {
   const router = useRouter();
+
   return (
     <>
       <Navbar scheme="white" />
       <div className="bg-white">
-      <AllServices />
-      <Footer />
+        <Suspense fallback={<div>Loading services...</div>}>
+          <AllServices />
+        </Suspense>
+
+        <Footer />
       </div>
     </>
   );
